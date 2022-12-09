caf930f76c03ca698977f878d2b7ba230873e414
connkat <hi.connkat@gmail.com>
2022-12-09 16:56:26 -0700

    cursors and text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logowhite from './images/logo-white.png';
+import logowhite from "./images/logo-white.png";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <img src={logowhite} className="logo" alt="logo" />
+      <div className="text">
+        <h1>Beatmaker &amp; DJ</h1>
+        <h1>Bass music, Halftime, Hip-Hop, House, Funk</h1>
+        <h1>
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfNC6UlAplGtnFhK9Wq9aknvYrlW-tP7fli00YqIF2cJKOecw/viewform?vc=0&c=0&w=1&flr=0"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Booking Request
+          </a>
+        </h1>
+      </div>
     </div>
   );
 }
