1f85c2141ed91408b08d57f76c0960cef3d942f0
connkat <hi.connkat@gmail.com>
2022-12-09 15:59:08 -0700

    background with logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import logowhite from './images/logo-white.png';
+import bg from './images/benanas1.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logowhite} className="logo" alt="logo" />
     </div>
   );
 }
