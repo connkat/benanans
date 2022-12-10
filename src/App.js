d77c3b4b10b55f516b9da5b5c94cec806766499b
Benanas <benference@gmail.com>
2022-12-10 12:59:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <img src={logowhite} className="logo" alt="logo" />
       <div className="text">
         <h1>Beatmaker &amp; DJ</h1>
-        <h1>Bass music, Halftime, Hip-Hop, House, Funk</h1>
+        <h3>Bass music, Halftime, Hip-Hop, House, Funk</h3>
         <h1>
           <a
             href="https://docs.google.com/forms/d/e/1FAIpQLSfNC6UlAplGtnFhK9Wq9aknvYrlW-tP7fli00YqIF2cJKOecw/viewform?vc=0&c=0&w=1&flr=0"
