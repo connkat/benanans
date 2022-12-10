13c2d345aae06dd79fa254ded66d29cc59212803
connkat <hi.connkat@gmail.com>
2022-12-09 17:42:22 -0700

    socials and bio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logowhite from "./images/logo-white.png";
+import bandcamp from "./images/socials/bandcamp.png";
+import facebook from "./images/socials/facebook.png";
+import instagram from "./images/socials/instagram.png";
+import soundcloud from "./images/socials/soundcloud.png";
+import spotify from "./images/socials/spotify.png";
 
 import "./App.css";
+import CustomSidebar from "./Components/CustomSidebar";
 
 function App() {
   return (
     <div className="App">
+      <div className="sidebar">
+        <CustomSidebar />
+      </div>
       <img src={logowhite} className="logo" alt="logo" />
       <div className="text">
         <h1>Beatmaker &amp; DJ</h1>
@@ -18,6 +27,47 @@ function App() {
             Booking Request
           </a>
         </h1>
+      </div>
+      <div className="social">
+        <a
+          href="https://www.facebook.com/ItsBenanas"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={facebook} className="social-icon" alt="social-icon" />
+        </a>
+        <a
+          href="https://www.instagram.com/itsbenanas/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={instagram}
+            className="social-icon-insta"
+            alt="social-icon"
+          />
+        </a>
+        <a
+          href="https://benanas.bandcamp.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={bandcamp} className="social-icon" alt="social-icon" />
+        </a>
+        <a
+          href="https://soundcloud.com/benanas"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={soundcloud} className="social-icon" alt="social-icon" />
+        </a>
+        <a
+          href="https://open.spotify.com/artist/1Fv8WShM73yWzLV29yoYNg?si=UHm7XAY5RqC9_DdzInk9wA&"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={spotify} className="social-icon" alt="social-icon" />
+        </a>
       </div>
     </div>
   );
---
+++ src/Components/CustomSidebar.jsx
@@ -0,0 +1,42 @@
+import { Sidebar, useProSidebar } from "react-pro-sidebar";
+
+function CustomSidebar() {
+  const { collapseSidebar } = useProSidebar();
+
+  return (
+    <div style={{ display: "flex", height: "100%", position: "absolute", right: 0, flexDirection: "row-reverse"}}>
+      <Sidebar>
+        <p>
+          Benanas is hot off a busy music festival season that took him all over
+          Alberta, British Columbia, and Saskatchewan. With original productions
+          under his belt, a record label of his own, and a few years off to hone
+          his style on the decks, make beats on his MPC, and sharpen his
+          library, Benanas made waves on dancefloors across the west coast music
+          festival scene day and night.
+        </p>
+        <p>
+          But this banana ain’t green... he’s be en attending west coast
+          festivals for the last 20 years, during which he’s amassed a track
+          pool deeper than an Olympic high dive. Benanas has been performing,
+          hosting, and promoting in Calgary’s best venues for the past decade.
+          His entirely unique blend of Bass music, Halftime, Hip - Hop, House,
+          Funk, Footwork and everything in between crafts a vibey flow that has
+          kept crowds moving at Canadian west coast clubs and music festivals
+          for years!{" "}
+        </p>
+        <p>
+          He’s a seasoned DJ and developing producer that can read a crowd and
+          give them exactly what they needed with a creative flair sprinkled on
+          top. You’re sure to get four things when seeing a live set: balanced
+          blends, tasteful turntablism, viscous vibes, and of course, that
+          sweet, sweet potassium
+        </p>
+      </Sidebar>
+      <main>
+        <button onClick={() => collapseSidebar()} style={{fontSize: "4vh", opacity: 0.5}}>Bio</button>
+      </main>
+    </div>
+  );
+}
+
+export default CustomSidebar;
